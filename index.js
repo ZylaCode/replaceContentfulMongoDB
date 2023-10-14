175e955903c7bd78bb6ee78bd078682e24d65d7f
Anna Basarab <90757241+AnnaBasarab93@users.noreply.github.com>
2023-10-14 18:09:52 +0200

    all without handleErrors

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ import express from 'express';
 import 'dotenv/config';
 import client from './db/client.js';
 import writersRouter from './routes/writers.js';
-
+import cors from 'cors';
 const port = process.env.PORT || 8000;
 
+
 const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:5173/",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+    credentials: true,
+  })
+)
 app.use(express.json());
+
 app.use('/api/writers', writersRouter);
 
 app.get('/', (req, res) => {
--- routes/writers.js
+++ routes/writers.js
@@ -3,17 +3,25 @@ import express from 'express';
 
 const writersRouter = express.Router();
 
-writersRouter.get("/", async (req, res) => {
+const handleErrors = (err, req, res, next) => {
+    //Perform some action on the request or response
+    console.log(err);
+    const statusCode = err.statusCode || 500;
+    const statusMessage = err.message || "Internal server error";
+    res.status(statusCode).json({error: statusMessage})
+}
+
+writersRouter.get("/", async (req, res, next) => {
     try {
         const response = await Writer.find();
         res.json(response)
       //res.send('Hello')
     } catch(err){
-        res.status(500).json(err)
+        next()
     }
-});
+},handleErrors);
 
-writersRouter.get("/:id", async (req, res) => {
+writersRouter.get("/:id", async (req, res, next) => {
     try {
         const id = req.params.id
         const response = await Writer.findById(id);
@@ -21,24 +29,25 @@ writersRouter.get("/:id", async (req, res) => {
         res.status(404).json({message : "Writer doesn't found"})    
     } 
     res.json(response)
- 
+
     }catch(err){
-        res.status(500).json(err)
+        next();
     }
- })
- 
-writersRouter.post("/", async (req, res) => {
+}, handleErrors)
+
+writersRouter.post("/", async (req, res, next) => {
     try {
+        console.log("Hello World")
         const {authorName, authorImage, authorBiography, bookTitle, bookImage, bookInfo, bookURL} = req.body;
         const response = await Writer.create({authorName, authorImage, authorBiography, bookTitle, bookImage, bookInfo, bookURL});
+        console.log(response, 'response')
         res.json(response);
     } catch(err){
-        const errors = handleErrors(err);        
-        res.status(400).json({errors})
+        res.status(500).json(err)
+        console.log(err)
+     //   next();
     }
-});
-
-
+}, handleErrors);
 
 
 export default writersRouter;
