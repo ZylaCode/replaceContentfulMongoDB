a41303e20038c63fb002c4fb8679e707df57c84c
Anna Basarab <90757241+AnnaBasarab93@users.noreply.github.com>
2023-10-14 14:14:35 +0200

    added if conditional

--- routes/writers.js
+++ routes/writers.js
@@ -5,34 +5,40 @@ const writersRouter = express.Router();
 
 writersRouter.get("/", async (req, res) => {
     try {
-      const response = await Writer.find();
-      res.json(response)
+        const response = await Writer.find();
+        res.json(response)
       //res.send('Hello')
     } catch(err){
         res.status(500).json(err)
     }
 });
 
+writersRouter.get("/:id", async (req, res) => {
+    try {
+        const id = req.params.id
+        const response = await Writer.findById(id);
+    if(!response){
+        res.status(404).json({message : "Writer doesn't found"})    
+    } 
+    res.json(response)
+ 
+    }catch(err){
+        res.status(500).json(err)
+    }
+ })
+ 
 writersRouter.post("/", async (req, res) => {
     try {
         const {authorName, authorImage, authorBiography, bookTitle, bookImage, bookInfo, bookURL} = req.body;
-      const response = await Writer.create({authorName, authorImage, authorBiography, bookTitle, bookImage, bookInfo, bookURL});
-      res.json(response);
+        const response = await Writer.create({authorName, authorImage, authorBiography, bookTitle, bookImage, bookInfo, bookURL});
+        res.json(response);
     } catch(err){
         const errors = handleErrors(err);        
         res.status(400).json({errors})
     }
 });
 
-writersRouter.get("/:id", async (req, res) => {
-   try {
-        const id = req.params.id
-      const response = await Writer.findById(id);
-      res.json(response)
-    } catch(err){
-        res.status(500).json(err);
-    }
-});
+
 
 
 export default writersRouter;
